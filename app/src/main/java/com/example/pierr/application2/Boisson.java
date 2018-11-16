f68581922abbd28708ca454c26c64d9a183d765f
Pierre <pierredemol@hotmail.fr>
2018-11-16 16:49:12 +0100

    La liste de boisson est affichée.

--- app/src/main/java/com/example/pierr/application2/Boisson.java
+++ app/src/main/java/com/example/pierr/application2/Boisson.java
@@ -7,24 +7,27 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Boisson extends AppCompatActivity {
 
     private Button retour;
     private ListView listView;
-    String boissons[]= {"Rio","Nojito","Mojito","Ice Tea","Milk Shake","Green Smoothie","Fraisibulle","Limonade Framboise","Pink Margarita"};
+    String boissons[] = {"Rio", "Nojito", "Mojito", "Ice Tea", "Milk Shake", "Green Smoothie", "Fraisibulle", "Limonade Framboise", "Pink Margarita"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boisson);
 
+
         retour = findViewById(R.id.btn_retour);
         listView = findViewById(R.id.lw_boisson);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.activity_list_item,boissons);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Boisson.this, android.R.layout.simple_list_item_1, boissons);
         listView.setAdapter(adapter);
-        listView.setOnClickListener(new ChoixBoisson());
+        //listView.setOnClickListener(new ChoixBoisson());
 
 
         retour.setOnClickListener(new View.OnClickListener() {
@@ -34,12 +37,14 @@ public class Boisson extends AppCompatActivity {
             }
         });
 
-        class ChoixBoisson() implements AdapterView.OnItemClickListener {
+        /*class ChoixBoisson() implements AdapterView.OnItemClickListener {
             @Override
-            public void OnItemClick(AdapterView<?> parent, View view, int position, long id){
+            public void onItemClick(AdapterView<?> this.getParent()parent, View view, int position, long id) {
+                TextView textView = (TextView) findViewById(R.id.tv_boisson_choisi);
+                Toast.makeText(ActivityAdapter.this,textView.getText().toString(),Toast.LENGTH_SHORT).show();
 
             }
 
-        }
+        }*/
     }
 }
